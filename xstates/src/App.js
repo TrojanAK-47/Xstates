7f4ceb199a7c5396f041d9940d0a52419ea01cef
Rakshit Sharma <rs11113@gmail.com>
2024-08-30 12:11:11 +0530

    first commit

---
+++ xstates/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import State from "./States/State";
+import "./App.css";
+const App = () => {
+  return (
+    <div className='container'>
+      <State />
+    </div>
+  );
+};
+
+export default App;
